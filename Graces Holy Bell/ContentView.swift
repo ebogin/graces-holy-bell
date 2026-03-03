de5b185d3893799722be604f380bd9997638582a
Eric Bogin <ericbogin@gmail.com>
2026-03-02 16:44:00 -0800

    Initial Commit

---
+++ Graces Holy Bell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Graces Holy Bell
+//
+//  Created by Eric Bogin on 3/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Graces Holy Bell/Graces_Holy_BellApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graces_Holy_BellApp.swift
+//  Graces Holy Bell
+//
+//  Created by Eric Bogin on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Graces_Holy_BellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
